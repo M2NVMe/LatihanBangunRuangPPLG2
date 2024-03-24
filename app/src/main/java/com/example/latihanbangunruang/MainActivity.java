8348328cbaae51fd30d8284cd70d8d1e415a9ca9
M2NV <m2sjmail@gmail.com>
2024-03-24 10:56:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihanbangunruang/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.latihanbangunruang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText Username,PW;
+    Button Login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Username = findViewById(R.id.txtUSR);
+        PW = findViewById(R.id.txtPW);
+        Login = findViewById(R.id.btnLGN);
+
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            String Nama = "Bumi";
+            String PWS = "123";
+            @Override
+            public void onClick(View view) {
+                if (Username.getText().toString().equals(Nama) && PW.getText().toString().equals(PWS)) {
+                    Intent intent = new Intent(MainActivity.this, utama.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Gagal!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/latihanbangunruang/utama.java
@@ -0,0 +1,39 @@
+package com.example.latihanbangunruang;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.latihanbangunruang.databinding.ActivityUtamaBinding;
+
+public class utama extends AppCompatActivity {
+    ActivityUtamaBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityUtamaBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new ProfileFragment());
+        binding.bottomNavigationView.setOnItemReselectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.d2) {
+                replaceFragment(new d2Fragment());
+            } else if (itemId == R.id.d3) {
+                replaceFragment(new d3Fragment());
+            } else if (itemId == R.id.prof) {
+                replaceFragment(new ProfileFragment());
+            }
+        });
+
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame,fragment);
+        fragmentTransaction.commit();
+    }
+}
