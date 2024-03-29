a128a468a37579402b7a7f06ba16b6106007f042
M2NV <m2sjmail@gmail.com>
2024-03-29 20:51:41 +0700

    [BBW] Feature add/fix

---
+++ app/src/main/java/com/example/latihanbangunruang/Activity3D/tabung.java
@@ -0,0 +1,46 @@
+package com.example.latihanbangunruang.Activity3D;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.latihanbangunruang.R;
+
+public class tabung extends AppCompatActivity {
+
+    EditText jari, tinggi;
+    Button calk, bak;
+    TextView hasil;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabung);
+        jari = findViewById(R.id.jari);
+        tinggi = findViewById(R.id.tinggi);
+        calk = findViewById(R.id.calc);
+        hasil = findViewById(R.id.hasil);
+        bak = findViewById(R.id.back);
+
+        calk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double hsl = 0;
+                float jr = Float.parseFloat(jari.getText().toString());
+                float tg = Float.parseFloat(tinggi.getText().toString());
+                double pi = 3.14;
+                hsl  = 2 * pi * jr * (jr + tg);
+                hasil.setText(String.valueOf(hsl));
+            }
+        });
+        bak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
