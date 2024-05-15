bd071cbd5a7ea19caf77c7ae1890efb0ea38730e
M2NV <m2sjmail@gmail.com>
2024-05-15 08:10:10 +0700

    [BBW] Feature add/fix

--- app/src/main/java/com/example/latihanbangunruang/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/latihanbangunruang/Fragments/ProfileFragment.java
@@ -1,6 +1,8 @@
 package com.example.latihanbangunruang.Fragments;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +16,8 @@ import com.example.latihanbangunruang.R;
 
 public class ProfileFragment extends Fragment {
     Button lgot;
+    SharedPreferences sharedPreferences;
+    private static final String SharedPref = "mypref";
     public ProfileFragment() {
 
     }
@@ -22,10 +26,13 @@ public class ProfileFragment extends Fragment {
 
         View RootView =  inflater.inflate(R.layout.fragment_profile, container, false);
         lgot = RootView.findViewById(R.id.Logut);
+        sharedPreferences = getContext().getSharedPreferences(SharedPref, Context.MODE_PRIVATE);
         lgot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                SharedPreferences.Editor Editor = sharedPreferences.edit();
+                Editor.clear();
+                Editor.apply();
                 getActivity().finishAffinity();
                 Intent intent = new  Intent(getActivity(), MainActivity.class);
                 startActivity(intent);
