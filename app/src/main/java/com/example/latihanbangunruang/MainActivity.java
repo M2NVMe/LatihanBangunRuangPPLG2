bdf74c3d6d21614b5216ea49075fa99b514f7a1b
M2NV <m2sjmail@gmail.com>
2024-05-15 07:51:42 +0700

    [BBW] Feature add/fix

--- app/src/main/java/com/example/latihanbangunruang/MainActivity.java
+++ app/src/main/java/com/example/latihanbangunruang/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.latihanbangunruang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
 
     EditText Username,PW;
     Button Login, RGR;
+    SharedPreferences sharedPreferences;
+    private static final String SharedPref = "mypref";
+    private static final String usernm = "name";
+    private static final String PWs = "paswod";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +26,12 @@ public class MainActivity extends AppCompatActivity {
         RGR = findViewById(R.id.btnRGR);
         PW = findViewById(R.id.txtPW);
         Login = findViewById(R.id.btnLGN);
-
+        sharedPreferences = getSharedPreferences(SharedPref,MODE_PRIVATE);
+        String nama = sharedPreferences.getString(usernm,null);
+        if (nama != null) {
+            Intent sukses = new Intent(MainActivity.this,utama.class);
+            startActivity(sukses);
+        }
 
         Login.setOnClickListener(new View.OnClickListener() {
             String Nama = "m2sjmail@gmail.com";
@@ -29,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (Username.getText().toString().equals(Nama) && PW.getText().toString().equals(PWS)) {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString(usernm,Username.getText().toString());
+                    editor.putString(PWs,PW.getText().toString());
+                    editor.apply();
+
                     Intent intent = new Intent(MainActivity.this, utama.class);
                     startActivity(intent);
                 } else {
